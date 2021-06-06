91d5b94c6f2670ab64de183bb86bb6483e95b917
limebinary <1a2w3d4r5g-[=]>
2021-06-06 08:35:31 +0800

    fix date object is None error

--- plotmanager/library/utilities/log.py
+++ plotmanager/library/utilities/log.py
@@ -5,6 +5,8 @@ import psutil
 import re
 import socket
 
+from datetime import datetime
+
 from plotmanager.library.utilities.instrumentation import increment_plots_completed
 from plotmanager.library.utilities.notifications import send_notifications
 from plotmanager.library.utilities.print import pretty_print_time
@@ -33,7 +35,10 @@ def _get_date_summary(analysis):
         if analysis['files'][file_path]['checked']:
             continue
         analysis['files'][file_path]['checked'] = True
-        end_date = analysis['files'][file_path]['data']['date'].date()
+        if analysis['files'][file_path]['data']['date'] is None:
+            end_date = datetime.now()
+        else:
+            end_date = analysis['files'][file_path]['data']['date'].date()
         if end_date not in summary:
             summary[end_date] = 0
         summary[end_date] += 1
